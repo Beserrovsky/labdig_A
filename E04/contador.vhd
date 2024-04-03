fcc73a51cc6552acc62d0ffe06247666f6ce4f4c
Renato <renato.ferreira@psdbrasil.com..br>
2024-04-02 23:50:10 -0300

    adiciona atribuição correta no reset

--- E04/contador.vhd
+++ E04/contador.vhd
@@ -26,29 +26,32 @@ use IEEE.std_logic_1164.ALL;
 -------------------------------------------------------------------------------
 
 entity contador is 
-port (
-    clk_i, rst_i, en_i   :  in bit; -- clock, reset, enable
-    counter_o            : out bit_vector(5 downto 0) -- 64 valores
-);
+    port (
+        clk_i, rst_i, en_i   :  in bit; -- clock, reset, enable
+        counter_o            : out bit_vector(5 downto 0) -- 64 valores
+    );
 end contador;
 
 -------------------------------------------------------------------------------
 
 architecture contador_arch of contador is
     
-    signal s_counter : bit_vector(5 downto 0);
+    signal s_counter : bit_vector(5 downto 0) := (others => '0');
 
 begin
 
-    process(clk_i, rst_i, en_i)
+    process(clk_i, rst_i)
+    begin
         if rst_i = '1' then
-            counter_o <= 0;
-        elsif (clk_i'event and clk_i='1') then
+            s_counter <= (others => '0');
+        elsif rising_edge(clk_i) then
             if en_i = '1' then
                 s_counter <= s_counter + 1;
             end if;
         end if;
     end process;
     
-  counter_o <= s_counter;
+    counter_o <= s_counter;
+
 end architecture;
+
